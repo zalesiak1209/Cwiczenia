6456fce54826c6af88f74a0321184fdfd8bd5772
PSulewski <zalesiak1209@wp.pl>
2021-11-08 13:32:51 +0100

    cwieczenia3

--- file_manager.py
+++
@@ -1,9 +0,0 @@
-class file_manager():
-    def __init__(self, file_name):
-        self.file_name = file_name
-
-    def read_file(self):
-        return self
-
-    def update_file(self, text_data):
-       self.file_name
